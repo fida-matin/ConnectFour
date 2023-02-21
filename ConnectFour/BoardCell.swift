e3a29f03a54a9c9914657baea39d01abe89b968e
Fida Matin <a1798239@student.adelaide.edu.au>
2023-02-22 08:33:07 +1030

    Initial Commit

---
+++ ConnectFour/BoardCell.swift
@@ -0,0 +1,13 @@
+//
+//  BoardCell.swift
+//  ConnectFour
+//
+//  Created by Fida Matin on 22/2/2023.
+//
+
+import UIKit
+
+class BoardCell: UICollectionViewCell
+{
+    @IBOutlet weak var image: UIImageView!
+}
